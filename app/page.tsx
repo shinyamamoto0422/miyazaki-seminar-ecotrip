b44a18048221709548b7738589df4c3039f19398
Shin.Yamamoto <yamashin0123@outlook.jp>
2024-02-13 00:03:01 +0900

    Remove unused code and dependencies

--- app/page.tsx
+++ app/page.tsx
@@ -1,20 +1,4 @@
 export default async function Home() {
-  const { stargazers_count: stars } = await fetch(
-    "https://api.github.com/repos/steven-tey/precedent",
-    {
-      ...(process.env.GITHUB_OAUTH_TOKEN && {
-        headers: {
-          Authorization: `Bearer ${process.env.GITHUB_OAUTH_TOKEN}`,
-          "Content-Type": "application/json",
-        },
-      }),
-      // data will revalidate every 24 hours
-      next: { revalidate: 86400 },
-    },
-  )
-    .then((res) => res.json())
-    .catch((e) => console.log(e));
-
   return (
     <div className="z-10 w-full max-w-xl px-5 pt-10 xl:px-0">
       <h1
